960a2440b34084480f4a436ccc674312dc463c70
Ambokile <ambokilekifukwe@gmail.com>
2014-08-13 07:01:07 -0700

    test

--- www/js/processor.js
+++ www/js/processor.js
@@ -39,3 +39,24 @@ xmlhttp.send();
 	    setTimeout(getUser,5);
 		
 }
+function getUser(){
+	    var xmlhttp;
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();}
+	else{
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");}
+ xmlhttp.onreadystatechange = function(){
+	 
+	 if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+		
+		 document.querySelector("#user").innerHTML = xmlhttp.responseText;
+		  document.querySelector("#user_media").innerHTML = xmlhttp.responseText;
+		   document.querySelector("#user_chat_room").innerHTML = xmlhttp.responseText;
+		 }
+                                        };
+xmlhttp.open("GET","getUser.php",true);
+xmlhttp.send();
+
+	}
+	    setTimeout(getUser,5);
+	   	

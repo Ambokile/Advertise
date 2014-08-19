acde876e976bb77f98742cb0c522612a360b41d4
Ambokile <ambokilekifukwe@gmail.com>
2014-08-19 10:14:51 -0700

    test

--- js/app.js
+++ js/app.js
@@ -5,19 +5,6 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic'])
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
- 
    var app = angular.module('add', ['ionic'])
    
 var application = angular.module('ionicApp', ['ionic'])

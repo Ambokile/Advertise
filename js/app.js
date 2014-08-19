8bb9cce6dd9cdff3e0990f2c1d300e7600769c4d
Ambokile <ambokilekifukwe@gmail.com>
2014-08-19 10:22:47 -0700

    test

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
+
 
    var app = angular.module('add', ['ionic'])
    
@@ -32,4 +32,5 @@ application.controller('InfoCtrl', function($scope) {
   angular.module('account', ['ionic'])
   angular.module('add_user', ['ionic'])
    angular.module('register', ['ionic'])
+   angular.module('home', ['ionic'])
 

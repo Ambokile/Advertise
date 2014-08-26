ec1ed7b89e2c4be7731fb5c0e5cbd5b79cd1ac1b
Ambokile <ambokilekifukwe@gmail.com>
2014-08-26 07:50:19 -0700

    test

--- js/processor.js
+++ js/processor.js
@@ -2,23 +2,7 @@
 	
  window.onload = function(){
 	  alert();
-	     function grap_info(){
-			
-	 var xmlhttp;
-	if(window.XMLHttpRequest){
-		xmlhttp = new XMLHttpRequest();}
-	else{
-	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");}
- xmlhttp.onreadystatechange = function(){
-	 if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-		document.querySelector("#info").innerHTML = xmlhttp.responseText;
-	 } 
-			 
-			                       };
-xmlhttp.open("GET","http://fishpond.site88.net/get_matangazo.php",true);
-xmlhttp.send();
-   }
-             setInterval(grap_info,5);
+	     
  }
       
  

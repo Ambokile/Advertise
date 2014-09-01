0fd5ef43ce76434181466784e3b7e610339d44e6
Ambokile <ambokilekifukwe@gmail.com>
2014-09-01 03:04:30 -0700

    test

--- js/app.js
+++ js/app.js
@@ -3,11 +3,6 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-
-
-   var app = angular.module('add', ['ionic'])
-   angular.module('add_1', ['ionic'])
-   
 var application = angular.module('ionicApp', ['ionic'])
 
 application.config(function($stateProvider, $urlRouterProvider) {
@@ -29,33 +24,6 @@ application.config(function($stateProvider, $urlRouterProvider) {
 application.controller('InfoCtrl', function($scope) {
   console.log('InfoCtrl');
 });
- 
- 
-var app_1 =  angular.module('home', ['ionic'])
-
-app_1.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: 'home.html',
-   // controller: 'HomeCtrl'
-  })
-  .state('register', {
-    url: '/register',
-    templateUrl: 'register.html',
-   // controller: 'HomeCtrl'
-  })
-  
-  $urlRouterProvider.otherwise("/");
-
-})
-app_1.controller('HomeCtrl', function($scope) {
-  console.log('HomeCtrl');
-});
-
-
-
    
    function ContentController($scope, $ionicSideMenuDelegate) {
   $scope.toggleLeft = function() {
